5d5525a4a499403551ac3a220c8632a22e558b23
Salil Sawal <salil@thenoiseinside.com>
2018-05-21 15:40:21 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { ApolloEngine } from 'apollo-engine';
 import bodyParser from 'body-parser';
+import compression from 'compression';
 import schema from './data/schema';
 
 const GRAPHQL_PORT = 3000;
+const ENGINE_API_KEY = 'service:salil2008-8580:l-eQ-jDFCCIfqb5K0Uf_Sg';
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+const engine = new ApolloEngine({
+  apiKey: ENGINE_API_KEY,
+  stores: [
+      {
+        name: 'inMemEmbeddedCache',
+        inMemory: {
+          cacheSize: 20971520 // 20 MB
+        }
+      }
+    ],
+    queryCache: {
+      publicFullQueryStore: 'inMemEmbeddedCache'
+    }
+});
+
+graphQLServer.use(compression());
+
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema, tracing: true, cacheControl: true }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-graphQLServer.listen(GRAPHQL_PORT, () =>
-  console.log(
-    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
-  )
-);
+engine.listen({
+  port: GRAPHQL_PORT,
+  expressApp: graphQLServer,
+});
